a40247647d12860d194ce9926c5bb35a2927f9ed
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2022-06-08 03:46:06 -0700

    brought back api key for deploy testing

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,7 +1,9 @@
+const API_KEY = "dacc51e700c24457a8c132500220706"
+
 
 const requests = {
-    fetchCurrentWeather: `current.json?key=${process.env.REACT_APP_API_KEY}`, 
-    fetchForecast: `forecast.json?key=${process.env.REACT_APP_API_KEY}`,
+    fetchCurrentWeather: `current.json?key=${API_KEY}`, 
+    fetchForecast: `forecast.json?key=${API_KEY}`,
     fetchUserIP: 'https://geolocation-db.com/json/'
 }
 
